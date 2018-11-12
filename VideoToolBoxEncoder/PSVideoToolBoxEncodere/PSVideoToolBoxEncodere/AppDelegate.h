a1cc4576135d634c5fd04dcccdf225bb6275cefc
liangpengshuai <pengshuai.liang@gmail.com>
2018-11-12 21:01:48 +0800

    init project

---
+++ VideoToolBoxEncoder/PSVideoToolBoxEncodere/PSVideoToolBoxEncodere/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PSVideoToolBoxEncodere
+//
+//  Created by 梁鹏帅 on 2018/11/12.
+//  Copyright © 2018 梁鹏帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoToolBoxEncoder/PSVideoToolBoxEncodere/PSVideoToolBoxEncodere/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  PSVideoToolBoxEncodere
+//
+//  Created by 梁鹏帅 on 2018/11/12.
+//  Copyright © 2018 梁鹏帅. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
