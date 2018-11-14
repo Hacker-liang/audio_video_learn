41ca6962183e65a86dd5f00d38331a96bb94facc
liangpengshuai <pengshuai.liang@gmail.com>
2018-11-14 21:16:07 +0800

    添加PSCameraRecord工程

---
+++ CameraRecord/PSCameraRecord/PSCameraRecord/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PSCameraRecord
+//
+//  Created by 梁鹏帅 on 2018/11/14.
+//  Copyright © 2018 梁鹏帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CameraRecord/PSCameraRecord/PSCameraRecord/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PSCameraRecord
+//
+//  Created by 梁鹏帅 on 2018/11/14.
+//  Copyright © 2018 梁鹏帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
